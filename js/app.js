bcd826eed7dc3a1fa4f1675646fd7c4fd2608d95
Adam Simonar <adamsimonar@Adams-MacBook-Pro.local>
2020-02-24 15:21:16 -0700

    Got scroll and nav menu built. Active class is updating properly. Project complete

--- js/app.js
+++ js/app.js
@@ -17,7 +17,8 @@
  * Define Global Variables
  * 
 */
-
+const body_sections = document.querySelectorAll("section");
+const nav_bar = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
@@ -25,7 +26,18 @@
  * 
 */
 
-
+/**
+ * Helper function used to check to see if a section is inside the viewport
+ */
+function isInViewport(section){
+    const section_size = section.getBoundingClientRect();
+    return (
+        section_size.top >= 0 &&
+        section_size.left >= 0 &&
+        section_size.bottom <= (window.innerHeight ||  document.documentElement.clientHeight) &&
+        section_size.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
 /**
  * End Helper Functions
@@ -33,14 +45,55 @@
  * 
 */
 
-// build the nav
+/**
+ * Function to create the nav bar and set event listeners to see if certain parts are clicked
+ * If clicked, scroll to that section.
+ */
+function create_nav(){
+    // build the nav
+    for (let i=0; i <body_sections.length; i++){
+        const currentSection = body_sections[i];
+        //check for attribute, future sections may not have the data-nav attribute.
+        if(currentSection.hasAttribute("data-nav")){
+            // create anchor
+            let anchor = document.createElement("a");
+            //create text inside of anchor
+            let text = document.createTextNode(currentSection.getAttribute("data-nav"))
+            //append text to anchor
+            anchor.appendChild(text);
 
+            //create list element
+            let li_element = document.createElement("li");
+            li_element.classList.add("menu__link");
+        
+            // Add scroll to section functionality when clicked
+            let sectionID = currentSection.getAttribute('id');
+            li_element.addEventListener('click', function scrollToView(){
+                document.getElementById(sectionID).scrollIntoView({behavior: 'smooth'});
+            });
+            //append anchor to li
+            li_element.appendChild(anchor);
+            //append li to nav bar
+            nav_bar.appendChild(li_element);
+        }
+    }
+}
 
 // Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
+/**
+ * Add class 'your-active-class' to the section being viewed.
+ * Uses helper function 'inInViewport' found in helper functions section.
+ */
+function addActiveClass(){
+    body_sections.forEach((section) => {
+        console.log(isInViewport(section));
+        if (isInViewport(section)){
+            section.classList.add('your-active-class');
+        } else {
+            section.classList.remove('your-active-class');
+        }
+    }, false);
+}
 
 /**
  * End Main Functions
@@ -49,9 +102,12 @@
 */
 
 // Build menu 
-
 // Scroll to section on link click
-
 // Set sections as active
-
-
+function run(){
+    // call these when page loads
+    create_nav();
+    window.addEventListener('scroll', addActiveClass);
+}
+// run functions
+document.addEventListener('DOMContentLoaded', run);
